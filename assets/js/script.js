4f0e44930ed19c7d5f0a237cfce1a592bd2a2875
Lucas Pereira Trindade <63750022+terabyt552@users.noreply.github.com>
2020-08-18 15:32:26 -0300

    API-PAISES

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,6 @@ var inputPrincipal = document.querySelector('#inputPrincipal');
 const navMenuPrincipal = document.querySelector('#navMenuPrincipal');
 const navbar = document.querySelector('.navbar i');
 const campoVazio = document.querySelector('.vazio');
-const ButtonUltimaPesquisa = document.querySelector('#ultimaPesquisa');
-var RespostaLocalStorage;
-var statusStorage;
 
 const paragrafoNome = document.querySelector('#name');
 const paragrafoLingFalada = document.querySelector('#lingFalada');
@@ -14,12 +11,20 @@ const paragrafoRegiao = document.querySelector('#regiao');
 const paragrafoCapital = document.querySelector('#capital');
 const paragrafoPopulaçao = document.querySelector('#populaçao');
 const paragrafoTerritorio = document.querySelector('#territorio');
+const paragrafoFronteiras = document.querySelector('#fronteiras');
+const paragrafoSimbolo = document.querySelector('#simbolo');
+const paragrafoCodigo = document.querySelector('#codigo');
+const paragrafoNomeMoeda = document.querySelector('#nameMoeda');
+const paragrafoGini = document.querySelector('#gini');
+const paragrafoFuso = document.querySelector('#fuso');
+const paragrafoBloco = document.querySelector('#blocos');
+
 
 var nomePais = 0;
 
 var opçoes = {
     method: 'GET',
-    mode: 'cors',
+    mode: 'no-cors',
     cache: 'default'
 }
 
@@ -74,6 +79,75 @@ function RespostaAPI() {
                 }
                 break;
 
+                case 'borders':
+                    let bordas = informaçoesPrincipais[campo];
+
+                     var resultadoBordas = "";
+                    for(let x in bordas) {
+                    resultadoBordas += bordas[x] + ", "
+                    }
+                break;
+
+                case 'currencies': 
+                    let moeda = informaçoesPrincipais[campo];
+                    moeda = moeda[0];
+
+                    var codigoMoeda = "";
+                    var nomeMoeda = "";
+                    var simboloMoeda = "";
+                   
+                for (let x in moeda) {
+                  
+                    switch (x) {
+
+                       case 'code':
+                           codigoMoeda = moeda[x];
+                        break;
+                   
+                        case 'name':
+                           nomeMoeda = moeda[x];
+                        break;
+
+                        case 'symbol':
+                            simboloMoeda = moeda[x];
+                        break;
+
+                   }  
+
+                }  
+                break;
+
+                case 'gini':
+                var gini = informaçoesPrincipais[campo];
+                break;
+
+                case 'timezones':
+                let fusoArray = informaçoesPrincipais[campo];
+
+                var fuso ="";
+                for(let x in fusoArray) {
+                    fuso += fusoArray[x] + ", "
+                }
+                break;
+
+                case 'regionalBlocs' :
+                    var regionalBlocsArray = informaçoesPrincipais[campo];
+
+                    regionalBlocsArray = regionalBlocsArray[0];
+
+                    for (let x in regionalBlocsArray) {
+                    switch(x) {
+                        case 'acronym': 
+                           var regionalBlocosAcronym = regionalBlocsArray[x];
+                        break;
+                        case 'name' :
+                            var regionalBlocosName = regionalBlocsArray[x];
+                    }
+                    
+
+                    var regionalBlocos = regionalBlocosAcronym + ",  " + regionalBlocosName;
+             
+                   }break;
            }
 
            paragrafoNome.textContent = nome;
@@ -82,6 +156,14 @@ function RespostaAPI() {
            paragrafoPopulaçao.textContent = populaçao;
            paragrafoTerritorio.textContent = territorio;
            paragrafoLingFalada.textContent = LinguaFalada;
+           paragrafoFronteiras.textContent = resultadoBordas;
+           paragrafoCodigo.textContent = codigoMoeda;
+           paragrafoSimbolo.textContent = simboloMoeda;
+           paragrafoNomeMoeda.textContent = nomeMoeda;
+           paragrafoGini.textContent = gini;
+           paragrafoFuso.textContent = fuso;
+           paragrafoBloco.textContent = regionalBlocos;
+
         }
 
 
@@ -93,31 +175,33 @@ function RespostaAPI() {
 
 var MensagemVazio = 0;
 
+
 buttonPesquisar.onclick = function () {
 
     nomePais = inputPrincipal.value;
     if(nomePais == 0){
         campoVazio.classList.add('mostrarVazio');
+        campoVazio.classList.add('animaçaoVibraçao');
         if(MensagemVazio == 0) {
             MensagemVazio = 1;
         }else {
             alert("Campo Vazio!")
         }
     }else{ 
-        localStorage.setItem('ultimaPesquisa', JSON.stringify(nomePais));
         campoVazio.classList.remove('mostrarVazio');
-    RespostaAPI();
+        RespostaAPI();
     }
 }
 
 inputPrincipal.onclick = function () {
-    ButtonUltimaPesquisa.textContent = localStorage.getItem('ultimaPesquisa');
-}
+    campoVazio.classList.remove('mostrarVazio');
+    campoVazio.classList.remove('animaçaoVibraçao');
+  }
+
 
 navbar.addEventListener('click', () => {
     navMenuPrincipal.classList.toggle('mostrar')
 })
-
 
 
 
