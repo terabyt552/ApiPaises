9bdc45060c8e73d09f3115a1d108a667b720e8bc
Lucas Pereira Trindade <63750022+terabyt552@users.noreply.github.com>
2020-08-24 12:22:34 -0300

    APIPAISES

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,14 @@ var inputPrincipal = document.querySelector('#inputPrincipal');
 const navMenuPrincipal = document.querySelector('#navMenuPrincipal');
 const navbar = document.querySelector('.navbar i');
 const campoVazio = document.querySelector('.vazio');
+const Letreiro = document.querySelector('#textoMovel');
+const logo = document.querySelector('#logoGlobo');
+const caixaMenssagem = document.querySelector('.caixaMenssagem');
+const pCaixaMenssagem = document.querySelector('.pCaixaMenssagem');
+const imgBandeira = document.querySelector('#imgBandeira');
+const main = document.querySelector("main");
+const sectionEntrada = document.querySelector('#sectionEntrada');
+const Referencias = document.querySelector('#Referencias');
 
 const paragrafoNome = document.querySelector('#name');
 const paragrafoLingFalada = document.querySelector('#lingFalada');
@@ -20,7 +28,13 @@ const paragrafoFuso = document.querySelector('#fuso');
 const paragrafoBloco = document.querySelector('#blocos');
 
 
+var nome;
+var codeAlpha;
 var nomePais = 0;
+
+
+
+inputPrincipal.value = localStorage.getItem('ultimaPesquisa');
 
 var opçoes = {
     method: 'GET',
@@ -39,6 +53,8 @@ function RespostaAPI() {
     }).catch(error => {"requisiçao negada" + error})
     }
 
+
+
     function implementandoRespostaAPI(Informaçoes) {
 
         informaçoesPrincipais = Informaçoes[0]
@@ -48,7 +64,8 @@ function RespostaAPI() {
            switch (campo) {
 
                case 'name':
-                var nome = informaçoesPrincipais[campo]
+                nome = informaçoesPrincipais[campo]
+                retornoFrasesMenssagem(2)
                 break;
 
                 case 'region':
@@ -77,6 +94,16 @@ function RespostaAPI() {
                             break;
                     }
                 }
+                break;
+
+                case 'alpha3Code' :
+                    codeAlpha = informaçoesPrincipais[campo];
+
+                break;
+
+                case 'flag' :
+                   var linkImgBandeira = informaçoesPrincipais[campo];
+
                 break;
 
                 case 'borders':
@@ -153,8 +180,8 @@ function RespostaAPI() {
            paragrafoNome.textContent = nome;
            paragrafoRegiao.textContent = regiao;
            paragrafoCapital.textContent = capital;
-           paragrafoPopulaçao.textContent = populaçao;
-           paragrafoTerritorio.textContent = territorio;
+           paragrafoPopulaçao.textContent = populaçao + " Pessoas";
+           paragrafoTerritorio.textContent = territorio + "km2";
            paragrafoLingFalada.textContent = LinguaFalada;
            paragrafoFronteiras.textContent = resultadoBordas;
            paragrafoCodigo.textContent = codigoMoeda;
@@ -163,6 +190,7 @@ function RespostaAPI() {
            paragrafoGini.textContent = gini;
            paragrafoFuso.textContent = fuso;
            paragrafoBloco.textContent = regionalBlocos;
+           imgBandeira.setAttribute('src', linkImgBandeira);
 
         }
 
@@ -171,14 +199,17 @@ function RespostaAPI() {
 
         
      }
-
-
 var MensagemVazio = 0;
+
+
 
 
 buttonPesquisar.onclick = function () {
 
+    sectionEntrada.style.display = "none";
+    main.style.display = "flex";
     nomePais = inputPrincipal.value;
+    localStorage.setItem('ultimaPesquisa', nomePais);
     if(nomePais == 0){
         campoVazio.classList.add('mostrarVazio');
         campoVazio.classList.add('animaçaoVibraçao');
@@ -193,17 +224,119 @@ buttonPesquisar.onclick = function () {
     }
 }
 
+
+
+
 inputPrincipal.onclick = function () {
+    retornoFrasesMenssagem(1);
     campoVazio.classList.remove('mostrarVazio');
     campoVazio.classList.remove('animaçaoVibraçao');
+    
   }
 
 
+
+
 navbar.addEventListener('click', () => {
-    navMenuPrincipal.classList.toggle('mostrar')
+    navMenuPrincipal.classList.toggle('mostrar');
+    retornoFrasesMenssagem(4);
 })
 
 
+
+
+logo.onmouseover = function () {
+    retornoFrasesMenssagem(3);
+}
+
+
+
+Referencias.onclick = function () {
+    main.style.display = "none";
+    sectionEntrada.style.display = "none";
+
+
+  }
+
+
+
+
+function retornoFrasesMenssagem(numeroFrase) {
+    if(numeroFrase == 1) {
+        caixaMenssagem.classList.add('mostrarCaixaMensagem');
+        pCaixaMenssagem.classList.add('mostrarPCaixaMenssagem');
+        pCaixaMenssagem.innerHTML = "Que país quer pesquisar?";  
+    }
+
+    if(numeroFrase == 2) {
+        let randow = Math.random();
+        randow = randow.toFixed(1);
+        pCaixaMenssagem.classList.add('mostrarPCaixaMenssagem');
+        caixaMenssagem.classList.add('mostrarCaixaMensagem');
+
+        switch (randow) {
+            case '0.1':
+                pCaixaMenssagem.innerHTML = codeAlpha + " é tudo de bom!";
+                break;
+        
+            case '0.2':
+                pCaixaMenssagem.innerHTML = codeAlpha;
+                break;
+            
+            case '0.3':
+                pCaixaMenssagem.innerHTML = codeAlpha + ",boas lembranças!";
+            break;
+
+            case '0.4':
+                pCaixaMenssagem.innerHTML = codeAlpha + "? Onde é que eu to?";
+                break;
+
+            case '0.5':
+                pCaixaMenssagem.innerHTML = "Fica na America?";
+                break;
+
+            case '0.5':
+                pCaixaMenssagem.innerHTML = "Esse país é demais!";
+                break;
+            
+            case '0.6':
+                pCaixaMenssagem.innerHTML = codeAlpha + ",gostaria de conhecer";
+            break;
+
+            case '0.7':
+                pCaixaMenssagem.innerHTML = "\__<1_1>__/";
+            break;
+
+            case '0.8':
+                pCaixaMenssagem.innerHTML = codeAlpha + "<=Esse ai é o 7x1?";
+            break;
+
+            case '0.9':
+                pCaixaMenssagem.innerHTML = codeAlpha + "!!";
+            break;
+        }
+    }
+
+    if(numeroFrase == 3) {
+        pCaixaMenssagem.classList.add('mostrarPCaixaMenssagem');
+        caixaMenssagem.classList.add('mostrarCaixaMensagem');
+        pCaixaMenssagem.innerHTML = "ISSO FAZ COSSEGAS!";
+    }
+
+    if(numeroFrase == 4) {
+        pCaixaMenssagem.classList.add('mostrarPCaixaMenssagem');
+        caixaMenssagem.classList.add('mostrarCaixaMensagem');
+        pCaixaMenssagem.innerHTML = "Ei! Cuidado com o que aperta! ";
+    }
+
+   
+    setTimeout(() => {
+       
+        caixaMenssagem.classList.remove('mostrarCaixaMensagem');
+        pCaixaMenssagem.classList.remove('mostrarPCaixaMenssagem');
+    },3000);
+  
+}
 
 
 
